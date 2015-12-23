97c9dd699d292481180b9430b9fdacdc91b1cec7
Emile Kroeger <flammifer@gmail.com>
2015-12-23 22:22:39 +0100

    New font, and better colors

--- app/static/js/surface-controller.js
+++ app/static/js/surface-controller.js
@@ -43,8 +43,22 @@ angular.module('infiniworld')
       return "rgb(" + r + ", " + g + ", " + b +")"
     }
     function blend(c1, c2, advance) {
-      
+      var result = [];
+      for (i = 0; i < 3; i++) {
+        result.push((1 - advance) * c1[i] + advance * c2[i]);
+      }
+      return result;
     }
+    function blendrgb(c1, c2, advance) {
+      blended = blend(c1, c2, advance);
+      return rgb(blended[0], blended[1], blended[2])
+    }
+    var DARKBLUE = [0.0, 0.0, 0.6];
+    var BLUE = [0, 0, 0.9];
+    var GREEN = [0, 1, 0];
+    var YELLOW = [1, 1, 0];
+    var GREY = [0.7, 0.7, 0.7];
+    
     var controller = function($scope) {
       var biome;
       var glyph = "??";
@@ -52,22 +66,30 @@ angular.module('infiniworld')
       if ($scope.altitude < 0.5) {
         biome = "sea";
         glyph = "~";
+        //var blended = blendrgb(DARKBLUE, BLUE, 2*$scope.altitude);
+        //style["background-color"] = rgb(0, 0, 0.7);
       } else {
         if ($scope.altitude > 0.9) {
           biome = "mountain";
           glyph = "◭";
+          var lowcolor = blend(YELLOW, GREEN, $scope.humidity);
+          var advance = 10 * ($scope.altitude - 0.9)
+          var blended = blendrgb(lowcolor, GREY, advance);
+          style["background-color"] = blended;
         } else {
           if ($scope.humidity >= 0.5) {
             biome = "forest";
-            glyph = "🌲";
+            if ($scope.altitude > 0.7) {
+              glyph = "⅊"; //coniferous🌲
+            } else {
+              glyph = "⅋" // deciduous
+            }
           } else {
             biome = "plains";
             glyph = "::";
           }
-          var r = 1 - $scope.humidity;
-          var g = 0.8;
-          var b = 0.2;
-          style["background-color"] = rgb(r, g, b);
+          var blended = blendrgb(YELLOW, GREEN, $scope.humidity);
+          style["background-color"] = blended;
         }
       }
       $scope.biome = biome;
