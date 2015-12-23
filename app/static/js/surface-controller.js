ad9284bc869b23bebe57998245b549bfefd5fdd4
Emile Kroeger <flammifer@gmail.com>
2015-12-23 14:47:10 +0100

    version with a map with basic terrain

--- app/static/js/surface-controller.js
+++ app/static/js/surface-controller.js
@@ -1,10 +1,15 @@
 angular.module('infiniworld')
   .service("sField", function() {
+    var seed = 1;
+    function random() {
+        var x = Math.sin(seed++) * 10000;
+        return x - Math.floor(x);
+    }
   	this.simpleMap = function(rows, cols) {
   		var map = {}
   		angular.forEach(rows, function(row) {
 	  		angular.forEach(cols, function(col) {
-	  			map[[row, col]] = Math.random()
+	  			map[[row, col]] = random()
 	  		});
   		});
   		return map;
@@ -14,18 +19,24 @@ angular.module('infiniworld')
   .directive("infiniworldCell", function() {
     return {
       scope: {
-        cellData: '=cellData',
+        altitude: '=altitude',
+        population: '=population',
+        temperature: '=temperature',
       },
-      template: 'cell({{10*cellData | number:0}})',
+      templateUrl: 'templates/surfacecell.html',
     };
   }) 
   .controller('SurfaceController', ['$scope', 'sField', function ($scope, sField) {
-  	$scope.rows = [0, 1, 2, 3, 4, 5];
-  	$scope.cols = [0, 1, 2, 3, 4, 5];
+  	$scope.rows = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  	$scope.cols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   	console.debug(sField);
   	console.debug(sField.simpleMap);
 
   	$scope.map = sField.simpleMap($scope.rows, $scope.cols);
+    // Eventually: make these correlated
+  	$scope.altitude = sField.simpleMap($scope.rows, $scope.cols);
+  	$scope.population = sField.simpleMap($scope.rows, $scope.cols);
+  	$scope.temperature = sField.simpleMap($scope.rows, $scope.cols);
     //$scope.getCell(x)
 
   }]);
