d594205dfdcfc1e619a05d9f0dac3be15768d9be
Emile Kroeger <flammifer@gmail.com>
2015-12-31 08:35:19 +0100

    make scroll handler more generic

--- app/static/js/home/home-controller.js
+++ app/static/js/home/home-controller.js
@@ -1,34 +1,36 @@
 angular.module('infiniworld')
-  .controller('HomeController', ['$scope', function ($scope) {
-    
-    
+  .service("sScrollControl", function() {
+    this.left = 0;
+    this.top = 0;
+    var dragInit = {
+      x: 0,
+      y: 0,
+    }
+    this.dragging = false;
+    this.handleMouseDown = function(e) {
+      //$scope.offset.left += 10;
+      this.dragging = true;
+      dragInit.x = e.clientX - this.left;
+      dragInit.y = e.clientY - this.top;
+    };
+    this.handleMouseMove = function(e) {
+      if (this.dragging) {
+        this.left = e.clientX - dragInit.x;
+        this.top = e.clientY - dragInit.y;
+        console.log(this.left);
+      }
+    };
+    this.handleMouseUp = function(e) {
+      this.dragging = false;
+    };    
+  })
+  .controller('HomeController', ['$scope', 'sScrollControl',
+  function($scope, sScrollControl) {
     // Experiment
     $scope.getColor = function(x, y) {
       return "rgb(100%, " + (10 - x) + "0%, " + (10 - y) + "0%)";
     }
     $scope.testrange = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-    $scope.offset = {
-      left: 0,
-      top: 0,
-    }
-    var dragInit = {
-      x: 0,
-      y: 0,
-    }
-    $scope.dragging = false;
-    $scope.handleMouseDown = function(e) {
-      //$scope.offset.left += 10;
-      $scope.dragging = true;
-      dragInit.x = e.clientX - $scope.offset.left;
-      dragInit.y = e.clientY - $scope.offset.top;
-    };
-    $scope.handleMouseMove = function(e) {
-      if ($scope.dragging) {
-        $scope.offset.left = e.clientX - dragInit.x;
-        $scope.offset.top = e.clientY - dragInit.y;
-      }
-    };
-    $scope.handleMouseUp = function(e) {
-      $scope.dragging = false;
-    };
+    // scroll handling
+    $scope.scroll = sScrollControl;
   }]);
