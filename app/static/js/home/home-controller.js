0c1254e80c296ad75e51600eaa6f3dc72faf2cb4
Emile Kroeger <flammifer@gmail.com>
2016-01-10 00:21:44 +0100

    Remove all debug crap from the welcome page, and have a brief introduction instead. Also have a link back home

--- app/static/js/home/home-controller.js
+++ app/static/js/home/home-controller.js
@@ -1,11 +1,5 @@
 angular.module('infiniworld')
-  .controller('HomeController', ['$scope', 'sScrollControl',
-  function($scope, sScrollControl) {
-    // Experiment
-    $scope.getColor = function(x, y) {
-      return "rgb(100%, " + (10 - x) + "0%, " + (10 - y) + "0%)";
-    }
-    $scope.testrange = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-    // scroll handling
-    $scope.scroll = sScrollControl;
+  .controller('HomeController', ['$scope',
+  function($scope) {
+    // Nothing, this is a static page.
   }]);
